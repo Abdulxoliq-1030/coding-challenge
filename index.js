3e20851e6cad4015c6c8c39fc7e3d0b5034b6906
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-10-02 15:41:44 +0500

    add new codes

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
 // const outputString = inputString.replace(/\./g, "[.]");
 // console.log(outputString); // 1[.]1[.]1[.]1
 
-const word = "I'm going to go home";
-const lastWord = word.split(" ").pop();
-const numberOfLetters = lastWord.length;
+// const word = "I'm going to go home";
+// const lastWord = word.split(" ").pop();
+// const numberOfLetters = lastWord.length;
 
-console.log(`${lastWord}'s word count is => ${numberOfLetters}`); // home => 4
+// console.log(`${lastWord}'s word count is => ${numberOfLetters}`); // home => 4
+
+function isPalindrome(word = "") {
+  console.log(
+    word === word.split("").reverse().join("") ? "Palindrome" : "Not Palindrome"
+  );
+}
+
+isPalindrome("kiyik");
+
+const word = "Hheee";
